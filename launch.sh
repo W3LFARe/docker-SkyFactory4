a8f1268e02c28c18eb75b5d2932669c640850b01
Roger <rwm810@gmail.com>
2025-01-07 00:23:47 -0500

    new format test

--- launch.sh
+++ launch.sh
@@ -2,38 +2,42 @@
 
 set -x
 
-SERVER_VERSION=5.0.2
+if ! [[ -w "/data" ]]; then
+  echo "Directory is not writable, check permissions for /mnt/user/appdata/vaulthunters3"
+  exit 66
+fi
+
+FORGE_VERSION=1.20.1--47.3.0
+# https://maven.minecraftforge.net/net/minecraftforge/forge/1.18.2-40.1.61/forge-1.18.2-40.1.61-installer.jar
 
 cd /data
 
-if ! [[ "$EULA" = "false" ]] || grep -i true eula.txt; then
+if ! [[ "$EULA" = "false" ]]; then
 	echo "eula=true" > eula.txt
 else
-	echo "You must accept the EULA by in the container settings."
-	exit 9
+	echo "You must accept the EULA to install."
+	exit 99
 fi
+
 
 if ! [[ -f Server-Files-$SERVER_VERSION.zip ]]; then
-	rm -fr config defaultconfigs global_data_packs global_resource_packs mods packmenu SkyFactory_4_Server_4*.zip
-	curl -Lo Server-Files-$SERVER_VERSION.zip 'https://edge.forgecdn.net/files/6044/657/SkyFactory_5_Server_5.0.2.zip' && unzip -u -o Server-Files-$SERVER_VERSION.zip -d /data
-	chmod +x Install.sh
-	./Install.sh
+  rm -fr config mods defaultconfigs scripts forge-*.jar start.sh *Server.zip
+  curl -Lo Server-Files-$SERVER_VERSION.zip 'https://edge.forgecdn.net/files/6044/657/SkyFactory_5_Server_5.0.2.zip' && unzip -u -o Server-Files-$SERVER_VERSION.zip -d /data
+  curl -Lo forge-${FORGE_VERSION}-installer.jar 'https://maven.minecraftforge.net/net/minecraftforge/forge/'${FORGE_VERSION}'/forge-'${FORGE_VERSION}'-installer.jar'
+  java -jar forge-${FORGE_VERSION}-installer.jar --installServer && rm -f forge-${FORGE_VERSION}-installer.jar
 fi
 
-if [[ -n "$MOTD" ]]; then
-    sed -i "/motd\s*=/ c motd=$MOTD" /data/server.properties
+if [[ -n "$JVM_OPTS" ]]; then
+  sed -i '/-Xm[s,x]/d' user_jvm_args.txt
+  for j in ${JVM_OPTS}; do sed -i '$a\'$j'' user_jvm_args.txt; done
 fi
-if [[ -n "$LEVEL" ]]; then
-    sed -i "/level-name\s*=/ c level-name=$LEVEL" /data/server.properties
-fi
+
 if [[ -n "$OPS" ]]; then
     echo $OPS | awk -v RS=, '{print}' > ops.txt
 fi
-if [[ -n "$ALLOWLIST" ]]; then
-    echo $ALLOWLIST | awk -v RS=, '{print}' > white-list.txt
-fi
 
-. ./settings.sh
-JVM_OPTS = $JVM_OPTS $JAVA_PARAMETERS
-curl -Lo log4j2_112-116.xml https://launcher.mojang.com/v1/objects/02937d122c86ce73319ef9975b58896fc1b491d1/log4j2_112-116.xml
-java -server $JVM_OPTS -Dfml.queryResult=confirm -Dlog4j.configurationFile=log4j2_112-116.xml -jar $SERVER_JAR nogui
+sed -i 's/server-port.*/server-port=25565/g' server.properties
+
+chmod 755 run.sh
+
+./run.sh
