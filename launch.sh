8e906bd38e02fd2834ef74f337fa227bd834322d
Roger <rwm810@gmail.com>
2025-01-07 00:01:53 -0500

    testing

--- launch.sh
+++ launch.sh
@@ -2,94 +2,38 @@
 
 set -x
 
-FORGE_VERSION=47.3.0
 SERVER_VERSION=5.0.2
+
 cd /data
 
-if ! [[ "$EULA" = "false" ]]; then
+if ! [[ "$EULA" = "false" ]] || grep -i true eula.txt; then
 	echo "eula=true" > eula.txt
 else
-	echo "You must accept the EULA to install."
-	exit 99
+	echo "You must accept the EULA by in the container settings."
+	exit 9
 fi
 
-if ! [[ -f "Server-Files-$SERVER_VERSION.zip" ]]; then
-	rm -fr config defaultconfigs kubejs mods packmenu Simple.zip forge*
-	curl -Lo "Server-Files-$SERVER_VERSION.zip" 'https://edge.forgecdn.net/files/6044/657/SkyFactory_5_Server_5.0.2.zip' || exit 9
-	unzip -u -o "Server-Files-$SERVER_VERSION.zip" -d /data
-	DIR_TEST=$(find . -type d -maxdepth 1 | tail -1 | sed 's/^.\{2\}//g')
-	if [[ $(find . -type d -maxdepth 1 | wc -l) -gt 1 ]]; then
-		cd "${DIR_TEST}"
-		find . -type d -exec chmod 777 {} +
-		mv -f * /data
-		cd /data
-		rm -fr "$DIR_TEST"
-	fi
-	
-	curl -Lo forge-${FORGE_VERSION}-installer.jar https://maven.minecraftforge.net/net/minecraftforge/forge/1.20.1-$FORGE_VERSION/forge-1.20.1-$FORGE_VERSION-installer.jar
-	java -jar forge-${FORGE_VERSION}-installer.jar --installServer
+if ! [[ -f Server-Files-$SERVER_VERSION.zip ]]; then
+	rm -fr config defaultconfigs global_data_packs global_resource_packs mods packmenu SkyFactory_4_Server_4*.zip
+	curl -Lo Server-Files-$SERVER_VERSION.zip 'https://edge.forgecdn.net/files/6044/657/SkyFactory_5_Server_5.0.2.zip' && unzip -u -o Server-Files-$SERVER_VERSION.zip -d /data
+	chmod +x Install.sh
+	./Install.sh
+fi
+
+if [[ -n "$MOTD" ]]; then
+    sed -i "/motd\s*=/ c motd=$MOTD" /data/server.properties
 fi
 if [[ -n "$LEVEL" ]]; then
     sed -i "/level-name\s*=/ c level-name=$LEVEL" /data/server.properties
 fi
-if [[ -n "$JVM_OPTS" ]]; then
-	sed -i '/-Xm[s,x]/d' user_jvm_args.txt
-	for j in ${JVM_OPTS}; do sed -i '$a\'$j'' user_jvm_args.txt; done
+if [[ -n "$OPS" ]]; then
+    echo $OPS | awk -v RS=, '{print}' > ops.txt
 fi
-if [[ -n "$MOTD" ]]; then
-    sed -i "s/motd\s*=/ c motd=$MOTD" /data/server.properties
-fi
-if [[ -n "$ENABLE_WHITELIST" ]]; then
-    sed -i "s/white-list=.*/white-list=$ENABLE_WHITELIST/" /data/server.properties
+if [[ -n "$ALLOWLIST" ]]; then
+    echo $ALLOWLIST | awk -v RS=, '{print}' > white-list.txt
 fi
 
-[[ ! -f whitelist.json ]] && echo "[]" > whitelist.json
-IFS=',' read -ra USERS <<< "$WHITELIST_USERS"
-for raw_username in "${USERS[@]}"; do
-	username=$(echo "$raw_username" | xargs)
-	if [[ ! "$username" =~ ^[a-zA-Z0-9_]{3,16}$ ]]; then
-		echo "Whitelist: Invalid username: '$username'. Skipping..."
-		continue
-	fi
-
-	UUID=$(curl -s "https://api.mojang.com/users/profiles/minecraft/$username" | jq -r '.id')
-	if [[ "$UUID" != "null" ]]; then
-		if jq -e ".[] | select(.uuid == \"$UUID\")" whitelist.json > /dev/null; then
-			echo "Whitelist: $username ($UUID) is already whitelisted."
-		else
-			UUID=$(echo "$UUID" | sed -r 's/(.{8})(.{4})(.{4})(.{4})(.{12})/\1-\2-\3-\4-\5/')
-			echo "Whitelist: Adding $username ($UUID) to whitelist."
-			jq ". += [{\"uuid\": \"$UUID\", \"name\": \"$username\"}]" whitelist.json > tmp.json && mv tmp.json whitelist.json
-		fi
-	else
-		echo "Whitelist: Failed to fetch UUID for $username."
-	fi
-done
-
-[[ ! -f ops.json ]] && echo "[]" > ops.json
-IFS=',' read -ra OPS <<< "$OP_USERS"
-for raw_username in "${OPS[@]}"; do
-    username=$(echo "$raw_username" | xargs)
-    if [[ ! "$username" =~ ^[a-zA-Z0-9_]{3,16}$ ]]; then
-        echo "Ops: Invalid username: '$username'. Skipping..."
-        continue
-    fi
-
-    UUID=$(curl -s "https://api.mojang.com/users/profiles/minecraft/$username" | jq -r '.id')
-    if [[ "$UUID" != "null" ]]; then
-        if jq -e ".[] | select(.uuid == \"$UUID\")" ops.json > /dev/null; then
-            echo "Ops: $username ($UUID) is already an operator."
-        else
-			UUID=$(echo "$UUID" | sed -r 's/(.{8})(.{4})(.{4})(.{4})(.{12})/\1-\2-\3-\4-\5/')
-            echo "Ops: Adding $username ($UUID) as operator."
-            jq ". += [{\"uuid\": \"$UUID\", \"name\": \"$username\", \"level\": 4, \"bypassesPlayerLimit\": false}]" ops.json > tmp.json && mv tmp.json ops.json
-        fi
-    else
-        echo "Ops: Failed to fetch UUID for $username."
-    fi
-done
-
-sed -i 's/server-port.*/server-port=25565/g' server.properties
-chmod 755 run.sh
-
-./run.sh
+. ./settings.sh
+JVM_OPTS = $JVM_OPTS $JAVA_PARAMETERS
+curl -Lo log4j2_112-116.xml https://launcher.mojang.com/v1/objects/02937d122c86ce73319ef9975b58896fc1b491d1/log4j2_112-116.xml
+java -server $JVM_OPTS -Dfml.queryResult=confirm -Dlog4j.configurationFile=log4j2_112-116.xml -jar $SERVER_JAR nogui
