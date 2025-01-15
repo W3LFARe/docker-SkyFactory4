827b27e41657c089df0cb859f702e7c5985255a0
W3LFARe <rwm810@gmail.com>
2025-01-15 04:00:07 -0500

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -8,7 +8,7 @@ if ! [[ -w "/data" ]]; then
 fi
 
 FORGE_VERSION=1.20.1-47.3.0
-SERVER_VERSION=5.0.5
+SERVER_VERSION={{SERVER_VERSION}}
 # https://maven.minecraftforge.net/net/minecraftforge/forge/1.18.2-40.1.61/forge-1.18.2-40.1.61-installer.jar
 
 cd /data
@@ -22,7 +22,7 @@ fi
 
 if ! [[ -f Server-Files-${SERVER_VERSION}.zip ]]; then
   rm -fr config mods defaultconfigs scripts forge-*.jar start.sh *Server.zip
-  curl -Lo Server-Files-${SERVER_VERSION}.zip 'https://edge.forgecdn.net/files/6085/603/SkyFactory5-5.0.5.zip'
+  curl -Lo Server-Files-${SERVER_VERSION}.zip '{{SERVER_ZIP_URL}}'
   curl -Lo forge-${FORGE_VERSION}-installer.jar 'https://maven.minecraftforge.net/net/minecraftforge/forge/'${FORGE_VERSION}'/forge-'${FORGE_VERSION}'-installer.jar'
   java -jar forge-${FORGE_VERSION}-installer.jar --installServer && rm -f forge-${FORGE_VERSION}-installer.jar
 fi
@@ -50,43 +50,4 @@ for raw_username in "${USERS[@]}"; do
     fi
 
     UUID=$(curl -s "https://api.mojang.com/users/profiles/minecraft/$username" | jq -r '.id')
-    if [[ "$UUID" != "null" ]]; then
-        if jq -e ".[] | select(.uuid == \"$UUID\")" whitelist.json > /dev/null; then
-            echo "Whitelist: $username ($UUID) is already whitelisted."
-        else
-            UUID=$(echo "$UUID" | sed -r 's/(.{8})(.{4})(.{4})(.{4})(.{12})/\1-\2-\3-\4-\5/')
-            echo "Whitelist: Adding $username ($UUID) to whitelist."
-            jq ". += [{\"uuid\": \"$UUID\", \"name\": \"$username\"}]" whitelist.json > tmp.json && mv tmp.json whitelist.json
-        fi
-    else
-        echo "Whitelist: Failed to fetch UUID for $username."
-    fi
-done
-
-[[ ! -f ops.json ]] && echo "[]" > ops.json
-IFS=',' read -ra OPS <<< "$OP_USERS"
-for raw_username in "${OPS[@]}"; do
-    username=$(echo "$raw_username" | xargs)
-    if [[ ! "$username" =~ ^[a-zA-Z0-9_]{3,16}$ ]]; then
-        echo "Ops: Invalid username: '$username'. Skipping..."
-        continue
-    fi
-
-    UUID=$(curl -s "https://api.mojang.com/users/profiles/minecraft/$username" | jq -r '.id')
-    if [[ "$UUID" != "null" ]]; then
-        if jq -e ".[] | select(.uuid == \"$UUID\")" ops.json > /dev/null; then
-            echo "Ops: $username ($UUID) is already an operator."
-        else
-            UUID=$(echo "$UUID" | sed -r 's/(.{8})(.{4})(.{4})(.{4})(.{12})/\1-\2-\3-\4-\5/')
-            echo "Ops: Adding $username ($UUID) as operator."
-            jq ". += [{\"uuid\": \"$UUID\", \"name\": \"$username\", \"level\": 4, \"bypassesPlayerLimit\": false}]" ops.json > tmp.json && mv tmp.json ops.json
-        fi
-    else
-        echo "Ops: Failed to fetch UUID for $username."
-    fi
-done
-
-sed -i 's/server-port.*/server-port=25565/g' server.properties
-chmod 755 run.sh
-
-./run.sh
+    if [[ "$UUID" != "null" ]];
