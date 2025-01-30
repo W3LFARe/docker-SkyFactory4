3964ee359462f6a9d2aec2df8a2e3ffec4c1a297
w3lfare <rwm810@gmail.com>
2025-01-30 19:09:29 +0000

    Update launch.sh and Dockerfile with latest server file info

--- launch.sh
+++ launch.sh
@@ -5,8 +5,8 @@ if ! [[ -w "/data" ]]; then
   exit 66
 fi
 
-FORGE_VERSION=
-SERVER_VERSION=
+FORGE_VERSION=1.20.1-47.3.0
+SERVER_VERSION=5.0.7
 # https://maven.minecraftforge.net/net/minecraftforge/forge/1.18.2-40.1.61/forge-1.18.2-40.1.61-installer.jar
 
 cd /data
